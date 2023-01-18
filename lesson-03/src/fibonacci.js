af0baa69707adb3bcf8c94d2a49c53959e35bd85
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-18 13:23:42 -0300

    feat: spies

---
+++ lesson-03/src/fibonacci.js
@@ -0,0 +1,7 @@
+module.exports = class Fibonacci {
+  *execute(input, current = 0, next = 1) {
+    if (input === 0) return 0;
+    yield current;
+    yield* this.execute(input - 1, next, current + next)
+  }
+}
