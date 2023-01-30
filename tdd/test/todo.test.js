c22b456bd5e20dbd4372b4981b8b9d39d30482b7
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-30 16:26:11 -0300

    test(tdd): Todo isValid

---
+++ tdd/test/todo.test.js
@@ -0,0 +1,40 @@
+const { describe, it, before } = require('mocha');
+const { expect } = require('chai');
+const Todo = require('../src/todo');
+
+describe('todo', () => {
+  describe('#isValid', () => {
+    it('should return "false" when creating an object without title/text', () => {
+      const data = {
+        text: '',
+        when: new Date()
+      };
+
+      const todo = new Todo(data);
+      const result = todo.isValid();
+      expect(result).to.be.not.ok;
+    });
+
+    it('should return "false" when creating an object using "when" property invalid', () => {
+      const data = {
+        text: 'Wash dishes',
+        when: new Date("foo")
+      };
+
+      const todo = new Todo(data);
+      const result = todo.isValid();
+      expect(result).to.be.not.ok;
+    });
+
+    it('should have "id", "text", "when" and "status" properties after created the object', () => {
+      const data = {
+        text: 'Wash dishes',
+        when: new Date()
+      };
+
+      const todo = new Todo(data);
+      const result = todo.isValid();
+      expect(result).to.be.ok;
+    });
+  });
+});
