5e480b7d00f72524f8562546f6e3f03948acf44e
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-30 21:21:17 -0300

    test(tdd): shouldn't save invalid data

--- tdd/test/todoService.test.js
+++ tdd/test/todoService.test.js
@@ -2,6 +2,7 @@ const { describe, it, before, beforeEach, afterEach } = require('mocha');
 const { expect } = require('chai');
 const { createSandbox } = require('sinon');
 const TodoService = require('../src/todoService');
+const Todo = require('../src/todo');
 
 describe('todoService', () => {
   let sandbox;
@@ -30,5 +31,48 @@ describe('todoService', () => {
 
       todoService = new TodoService(dependencies);
     });
+
+    it('should return data in a specific format', () => {
+      const result = todoService.list();
+      const [{ meta, $loki, ...expected }] = mockDatabase;
+      expect(result).to.be.deep.equal([expected]);
+    });
+  });
+
+  describe('#create', () => {
+    let todoService;
+    beforeEach(() => {
+      const dependencies = {
+        todoRepository: {
+          create: sandbox.stub().returns(true),
+        }
+      };
+
+      todoService = new TodoService(dependencies);
+    });
+
+    it('should not save todo item with invalid data', () => {
+      const data = new Todo({
+        text: '',
+        when: '',
+      });
+
+      Reflect.deleteProperty(data, 'id');
+      const expected = {
+        error: {
+          message: 'invalid data',
+          data
+        },
+      };
+
+      const result = todoService.create(data);
+      expect(result).to.be.deep.equal(expected);
+    });
+
+    it('should save todo item with late status when "date" property is before than today', () => {
+
+    });
+
+    it('should save todo item with pending status when "date" property is further than today', () => { });
   });
 });
